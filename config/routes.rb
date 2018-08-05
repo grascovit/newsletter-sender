df9ec02fec536df1a5366a6ab0678c4b93ccc99c
Gabriel Rascovit <rascovit@gmail.com>
2018-08-05 17:08:49 -0300

    separate root routes for authenticated and unauthenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root to: 'rails/welcome#index', as: :authenticated_root
+    end
+
+    unauthenticated :user do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
