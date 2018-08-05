388addc4c0372fa65d6a5a09a1519d26fa319512
Gabriel Rascovit <rascovit@gmail.com>
2018-08-05 17:46:54 -0300

    configure production smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,18 @@ Rails.application.configure do
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] }
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['MAILER_USERNAME'],
+    password: ENV['MAILER_PASSWORD'],
+    domain: ENV['MAILER_DOMAIN'],
+    address: ENV['MAILER_ADDRESS'],
+    port: ENV['MAILER_PORT'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
