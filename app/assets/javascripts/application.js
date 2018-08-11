d758a1a609f39b8805d1d143858b742f13ec199d
Gabriel Rascovit <rascovit@gmail.com>
2018-08-11 01:08:08 -0300

    add tinymce wysiwyg editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,17 @@
 //= require jquery3
 //= require popper
 //= require bootstrap
+//= require vendor/tinymce/tinymce.min
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+  tinymce.remove();
+  tinymce.init({
+    selector: '.tinymce',
+    language: 'pt_BR',
+    toolbar: 'undo redo | bold italic underline | ' +
+      'fontselect fontsizeselect forecolor backcolor | ' +
+      'alignleft aligncenter alignright alignjustify | ' +
+      'bullist numlist outdent indent | link image'
+  });
+});
