10b0f1d878a5aa4203f75b84a0307f1a923756ba
Gabriel Rascovit <rascovit@gmail.com>
2018-08-12 21:51:31 -0300

    differentiate flash messages colors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  FLASH_TYPES = {
+    alert: 'alert-danger',
+    notice: 'alert-primary'
+  }.freeze
+
   def active_item(name)
     'active' if name == controller_name
+  end
+
+  def flash_class(type)
+    FLASH_TYPES[type.to_sym]
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -22,4 +22,20 @@ RSpec.describe ApplicationHelper, type: :helper do
       it { is_expected.to be_nil }
     end
   end
+
+  describe '#flash_class' do
+    subject { helper.flash_class(type) }
+
+    context 'when flash type is alert' do
+      let(:type) { 'alert' }
+
+      it { is_expected.to eq('alert-danger') }
+    end
+
+    context 'when flash type is notice' do
+      let(:type) { 'notice' }
+
+      it { is_expected.to eq('alert-primary') }
+    end
+  end
 end
