389a0c977b0907cf6c65d017b0dc02ed650c8af6
Gabriel Rascovit <rascovit@gmail.com>
2020-02-07 17:55:29 -0300

    setup vps deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: ENV['DEFAULT_HOST'] }
   config.action_mailer.delivery_method = :smtp
+  config.force_ssl = true
 
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['MAILER_USERNAME'],
